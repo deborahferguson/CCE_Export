e2aec8ca05fd93c649abe826af0e31066a6a09c8
Deborah Ferguson <dferg@login3.frontera.tacc.utexas.edu>
2025-03-26 11:25:27 -0500

    start exporting to h5

--- src/cce_export.cc
+++ src/cce_export.cc
@@ -1,5 +1,36 @@
 #include "cce_export.hh"
 #include <vector>
+//#define H5_USE_16_API
+//#include <hdf5.h>
+#include <sys/stat.h>
+#include <iomanip>
+#include <string.h>
+
+#ifdef HAVE_CAPABILITY_HDF5
+// We currently support the HDF5 1.6 API (and when using 1.8 the                                                                                                                                                                                                                                                                                                                           
+// compatibility mode introduced by H5_USE_16_API).  Several machines                                                                                                                                                                                                                                                                                                                      
+// in SimFactory use HDF5 1.6, so we cannot drop support for it.  It                                                                                                                                                                                                                                                                                                                       
+// seems it is hard to support both the 1.6 and 1.8 API                                                                                                                                                                                                                                                                                                                                    
+// simultaneously; for example H5Fopen takes a different number of                                                                                                                                                                                                                                                                                                                         
+// arguments in the two versions.                                                                                                                                                                                                                                                                                                                                                          
+#define H5_USE_16_API
+#include <hdf5.h>
+#endif
+
+
+
+#define HDF5_ERROR(fn_call)						\
+  do {                                                                  \
+    hid_t _error_code = fn_call;                                        \
+    									\
+    									\
+    if (_error_code < 0)                                                \
+    {									\
+      CCTK_VWarn (0, __LINE__, __FILE__, CCTK_THORNSTRING,              \
+		  "HDF5 call '%s' returned error code %d",              \
+		  #fn_call, (int)_error_code);                          \
+    }                                                                   \
+  } while (0)
 
 using namespace std;
 
@@ -19,10 +50,10 @@ CCTK_REAL CCE_Export_Simpson2DIntegral(CCTK_REAL const *f, int nx, int ny, CCTK_
   int px = nx / 2;
   int py = ny / 2;
 
-  // Corners                                                                                                                                                                                          
+  // Corners
   integrand_sum += f[idx(0,0)] + f[idx(nx,0)] + f[idx(0,ny)] + f[idx(nx,ny)];
 
-  // Edges                                                                                                                                                                                            
+  // Edges
   for (iy = 1; iy <= py; iy++)
     integrand_sum += 4 * f[idx(0,2*iy-1)] + 4 * f[idx(nx,2*iy-1)];
 
@@ -35,7 +66,7 @@ CCTK_REAL CCE_Export_Simpson2DIntegral(CCTK_REAL const *f, int nx, int ny, CCTK_
   for (ix = 1; ix <= px-1; ix++)
     integrand_sum += 2 * f[idx(2*ix,0)] + 2 * f[idx(2*ix,ny)];
 
-  // Interior                                                                                                                                                                                         
+  // Interior
   for (iy = 1; iy <= py; iy++)
     for (ix = 1; ix <= px; ix++)
       integrand_sum += 16 * f[idx(2*ix-1,2*iy-1)];
@@ -235,10 +266,6 @@ void Decompose_Spherical_Harmonics(vector<CCTK_REAL> &th, vector<CCTK_REAL> &phi
   }
 }
 
-// void index_to_l_m(int index, int &l, int &m){
-  
-// }
-
 int l_m_to_index(int l, int m)
 {
   return l*l + l + m;
@@ -297,6 +324,188 @@ void Compute_Ylms(vector<CCTK_REAL> &th, vector<CCTK_REAL> &ph, vector<vector<CC
     }
   }
 }
+
+#ifdef HAVE_CAPABILITY_HDF5
+
+static bool file_exists(const string &name)
+{
+  struct stat sts;
+  return !(stat(name.c_str(), &sts) == -1 && errno == ENOENT);
+}
+
+static bool dataset_exists(hid_t file, const string &dataset_name)
+{
+  // To test whether a dataset exists, the recommended way in API 1.6                                                                                                                                 
+  // is to use H5Gget_objinfo, but this prints an error to stderr if                                                                                                                                  
+  // the dataset does not exist.  We explicitly avoid this by wrapping                                                                                                                                
+  // the call in H5E_BEGIN_TRY/H5E_END_TRY statements.  In 1.8,                                                                                                                                       
+  // H5Gget_objinfo is deprecated, and H5Lexists does the job.  See                                                                                                                                   
+  // http://www.mail-archive.com/hdf-forum@hdfgroup.org/msg00125.html                                                                                                                                 
+
+  #if 1
+  bool exists;
+  H5E_BEGIN_TRY
+    {
+      exists = H5Gget_objinfo(file, dataset_name.c_str(), 1, NULL) >= 0;
+    }
+  H5E_END_TRY;
+  return exists;
+  #else
+  return H5Lexists(file, dataset_name.c_str(), H5P_DEFAULT);
+  #endif
+}
+	      
+void Output_Decomposed_Metric_Data(CCTK_ARGUMENTS, vector<vector<vector<CCTK_REAL>>> &re_g, vector<vector<vector<CCTK_REAL>>> &im_g, 
+				   vector<vector<vector<CCTK_REAL>>> &re_dr_g, vector<vector<vector<CCTK_REAL>>> &im_dr_g, 
+				   vector<vector<vector<CCTK_REAL>>> &re_dt_g, vector<vector<vector<CCTK_REAL>>> &im_dt_g, 
+				   vector<vector<CCTK_REAL>> &re_beta, vector<vector<CCTK_REAL>> &im_beta, 
+				   vector<vector<CCTK_REAL>> &re_dr_beta, vector<vector<CCTK_REAL>> &im_dr_beta, 
+				   vector<vector<CCTK_REAL>> &re_dt_beta, vector<vector<CCTK_REAL>> &im_dt_beta, 
+				   vector<CCTK_REAL> &re_alpha, vector<CCTK_REAL> &im_alpha, 
+				   vector<CCTK_REAL> &re_dr_alpha, vector<CCTK_REAL> &im_dr_alpha, 
+				   vector<CCTK_REAL> &re_dt_alpha, vector<CCTK_REAL> &im_dt_alpha, 
+				   float rad, int mode_count)
+{
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+
+  const char *my_out_dir = strcmp(out_dir, "") ? out_dir : io_out_dir;
+  if (CCTK_CreateDirectory(0755, my_out_dir) < 0)
+    CCTK_VWarn(CCTK_WARN_ABORT, __LINE__, __FILE__, CCTK_THORNSTRING,
+               "Multipole output directory %s could not be created",
+               my_out_dir);
+
+  static bool checked; // Has the given file been checked
+                                   // for truncation? bool
+                                   // defaults to false  
+
+  ostringstream basename;
+  basename << "CCE_Export_R" << setiosflags(ios::fixed) << setprecision(2) << rad << ".h5";
+  string output_name = my_out_dir + string("/") + basename.str();
+
+  printf(output_name.c_str());
+  printf("\n");
+
+  hid_t file;
+
+  if (!file_exists(output_name) || (!checked && IO_TruncateOutputFiles(cctkGH)))
+    {
+      file = H5Fcreate(output_name.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+    }
+  else
+    {
+      file = H5Fopen(output_name.c_str(), H5F_ACC_RDWR, H5P_DEFAULT);
+    }
+
+  checked = true;
+
+  ostringstream datasetname;
+  datasetname << "l" << 0 << "_m" << 0 << "_r" << setiosflags(ios::fixed) << setprecision(2) << 0;
+
+  hid_t dataset = -1;
+
+  if (dataset_exists(file, datasetname.str()))
+    {
+      dataset = H5Dopen(file, datasetname.str().c_str());
+    }
+  else
+    {
+      hsize_t dims[2] = {0,3};
+      hsize_t maxdims[2] = {H5S_UNLIMITED, 3};
+      hid_t dataspace = H5Screate_simple(2, dims, maxdims);
+
+      hid_t cparms = -1;
+      hsize_t chunk_dims[2] = {hsize_t(hdf5_chunk_size),3};
+      cparms = H5Pcreate (H5P_DATASET_CREATE);
+      HDF5_ERROR(H5Pset_chunk(cparms, 2, chunk_dims));
+
+      dataset = H5Dcreate(file, datasetname.str().c_str(), H5T_NATIVE_DOUBLE, dataspace, cparms);
+      H5Pclose(cparms);
+    }
+
+
+  // string datasetname = "gxx.dat";
+
+  // hid_t dataset = -1;
+  // printf(datasetname.c_str());
+  // printf("\n");
+
+  // if (dataset_exists(file, datasetname))
+  // {
+  //   dataset = H5Dopen(file, datasetname.c_str());
+  // }
+  // else
+  // {
+  //   hsize_t dims[2] = {0,3};
+  //   hsize_t maxdims[2] = {H5S_UNLIMITED, 3};
+  //   hid_t dataspace = H5Screate_simple(2, dims, maxdims);
+    
+  //   hid_t cparms = -1;
+  //   hsize_t chunk_dims[2] = {hsize_t(hdf5_chunk_size), 3};
+  //   cparms = H5Pcreate(H5P_DATASET_CREATE);
+  //   printf("Property list handle: %lld\n", (long long)cparms);
+  //   HDF5_ERROR(H5Pset_chunk(cparms, 2, chunk_dims));
+    
+  //   dataset = H5Dcreate(file, datasetname.c_str(), H5T_NATIVE_DOUBLE, dataspace, cparms);
+  //   H5Pclose(cparms);
+  // }
+  
+  hid_t filespace = H5Dget_space (dataset);
+  hsize_t filedims[2];
+  hsize_t maxdims[2];
+  HDF5_ERROR(H5Sget_simple_extent_dims(filespace, filedims, maxdims));
+
+  filedims[0] += 1;
+  hsize_t size[2] = {filedims[0], filedims[1]};
+  HDF5_ERROR(H5Dextend (dataset, size));
+  HDF5_ERROR(H5Sclose(filespace));
+
+  /* Select a hyperslab  */
+  hsize_t offset[2] = {filedims[0]-1, 0};
+  hsize_t dims2[2] = {1, 3};
+  filespace = H5Dget_space (dataset);
+  HDF5_ERROR(H5Sselect_hyperslab (filespace, H5S_SELECT_SET, offset, NULL,
+				  dims2, NULL));
+
+
+  // CCTK_REAL data[] = {cctk_time, modes(v, i, l, m, 0), modes(v, i, l, m, 1)};
+  CCTK_REAL data[] = {cctk_time, re_g[0][0][0], im_g[0][0][0]};
+  // data[0] = cctk_time;
+  // for(int mode_index=0; mode_index<mode_count; mode_index++)
+  // {
+  //   data[2*mode_index+1] = re_g[0][0][mode_index];
+  //   data[2*mode_index+2] = im_g[0][0][mode_index];
+  // }
+  
+  hid_t memdataspace = H5Screate_simple(2, dims2, NULL);
+
+  /* Write the data to the hyperslab  */
+  HDF5_ERROR(H5Dwrite (dataset, H5T_NATIVE_DOUBLE, memdataspace, filespace,
+		       H5P_DEFAULT, data));
+
+  HDF5_ERROR(H5Dclose(dataset));
+  HDF5_ERROR(H5Sclose(filespace));
+  HDF5_ERROR(H5Sclose(memdataspace));
+
+}
+
+#else
+
+void  Output_Decomposed_Metric_Data(CCTK_ARGUMENTS, vector<vector<vector<CCTK_REAL>>> &re_g, vector<vector<vector<CCTK_REAL>>> &im_g,
+				    vector<vector<vector<CCTK_REAL>>> &re_dr_g, vector<vector<vector<CCTK_REAL>>> &im_dr_g,
+				    vector<vector<vector<CCTK_REAL>>> &re_dt_g, vector<vector<vector<CCTK_REAL>>> &im_dt_g,
+				    vector<vector<CCTK_REAL>> &re_beta, vector<vector<CCTK_REAL>> &im_beta,
+				    vector<vector<CCTK_REAL>> &re_dr_beta, vector<vector<CCTK_REAL>> &im_dr_beta,
+				    vector<vector<CCTK_REAL>> &re_dt_beta, vector<vector<CCTK_REAL>> &im_dt_beta,
+				    vector<CCTK_REAL> &re_alpha, vector<CCTK_REAL> &im_alpha,
+				    vector<CCTK_REAL> &re_dr_alpha, vector<CCTK_REAL> &im_dr_alpha,
+				    vector<CCTK_REAL> &re_dt_alpha, vector<CCTK_REAL> &im_dt_alpha,
+				    float rad, int mode_count)
+{
+  CCTK_WARN(0,"HDF5 output has been requested but Cactus has been compiled without HDF5 support");
+}
+
+#endif
 
 void CCE_Export(CCTK_ARGUMENTS)
 {
@@ -606,6 +815,9 @@ void CCE_Export(CCTK_ARGUMENTS)
     printf("Decomposed lapse data\n");
 
     // Store output in h5 file
+    Output_Decomposed_Metric_Data(CCTK_PASS_CTOC, re_g, im_g, re_dr_g, im_dr_g, re_dt_g, im_dt_g, 
+				  re_beta, im_beta, re_dr_beta, im_dr_beta, re_dt_beta, im_dt_beta, 
+				  re_alpha, im_alpha, re_dr_alpha, im_dr_alpha, re_dt_alpha, im_dt_alpha, radius[r], mode_count);
   }
 
 }
